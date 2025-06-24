08914989ef22db249660503f23c7fec37167d09e
Shakir Salam <shakirsalam555@Gmail.com>
2025-06-24 21:12:18 +0800

    ui orientation

--- web/main.js
+++
@@ -1,7 +0,0 @@
-let canvas = document.getElementById("canvas");
-    let ctx = canvas.getContext("2d");
-    let backend;
-
-    new QWebChannel(qt.webChannelTransport, function(channel) {
-        backend = channel.objects.backend;
-    });
